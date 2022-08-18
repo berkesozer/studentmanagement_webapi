3b6eae6a534a017154ef2d961f144f1c8cce59b5
Berke Sozer <berkesozerley@gmail.com>
2022-08-18 23:48:44 +0300

    Added role based authentication

--- studentmanagement_webapi/Program.cs
+++ studentmanagement_webapi/Program.cs
@@ -18,6 +18,7 @@ builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options => {
